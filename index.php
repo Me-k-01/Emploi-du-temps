a7eab51b76a85a9358b491b2af7589401fd0d5b9
Me-k-01 <mecaman01@gmail.com>
2021-09-16 19:23:33 +0200

    It just works

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php require_once 'header.php' ?>
 
-
   <body>
     <header>test</header>
     <?php
-    $servername = "51.77.202.155:3306";
+    $servername = "51.77.202.155";
     $username = "u48541_IxLPYgYqNg";
     $password = "E^3Jvy=ShXz!1u1CkB9y2Y.B";
-use PDO;
     // Create connection
-    $conn = new PDO("mysql:host=$servername;dbname=Test", $username, $password);
-
-    // Check connection
-    if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=s48541_Test", $username, $password);
+    } catch (Exeption $e) {
+     die("Connection failed: " . $e->getMessage());
     }
     echo "Connected successfully";
     ?>
