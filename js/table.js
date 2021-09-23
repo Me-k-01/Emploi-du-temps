919ad1a287c5168d9049dad6b7c089a627900c17
Me-k-01 <mecaman01@gmail.com>
2021-09-23 22:54:37 +0200

    invert table

--- js/table.js
+++ js/table.js
@@ -5,43 +5,46 @@ class Table{
     this.table = table;
     this.heureMin = 8;
     this.heureMax = 17;
-    this.setColTitle();
-    this.createRow();
+    this.create();
   }
 
-  setColTitle() {
-    const row = this.table.insertRow(0);
-    row.insertCell();
-    for (let h = this.heureMin; h <= this.heureMax; h++) {
-      row.insertCell(-1).outerHTML = `<th scope="col">${h}h</th>`;
-    }
-  }
-
-  createRow() {
+  create() {
     const days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
 
+    // Ligne des nom de jours
+    const titleRow = this.table.insertRow(0);
+    titleRow.insertCell(0)
     for (const day of days) {
+      titleRow.insertCell(-1).outerHTML = `<th scope="col" id="${day.toLowerCase()}">${day}</th>`;
+    }
+
+    for (let h = this.heureMin; h <= this.heureMax; h++) {
       const row = this.table.insertRow(-1);
-      const d = day.toLowerCase();
-      row.insertCell(0).outerHTML = `<th scope="row" id="${d}">${day}</th>`;
-      for (let h = this.heureMin; h <= this.heureMax; h++) {
-        row.insertCell(-1).outerHTML = `<td scope="row" id="${d}-${h}"></td>`;
+      // Colone des heures
+      row.insertCell(0).outerHTML = `<th scope="row" id="${h}">${h}</th>`;
+      // Remplissage
+      for (const day of days) {
+        row.insertCell(-1).outerHTML = `<td scope="row" id="${day.toLowerCase()}-${h}"></td>`;
       }
     }
   }
+
   getId({jour, horaire}) {
-    console.log(horaire);
     const h = parseInt(horaire.substring(0, 2));
     return jour.toLowerCase() + "-" + h;
   }
   set(id, div) {
-    console.log(id);
     doc.getElementById(id).appendChild(div);
   }
 
   static formatDOM({titre, salle}) {
     const container = doc.createElement('div');
-    container.append(doc.createTextNode(titre), doc.createTextNode(salle))
+    const title = doc.createElement('p');
+    title.innerHTML = titre;
+    const place = doc.createElement('p');
+    place.innerHTML = salle;
+
+    container.append(title, place);
     return container;
   }
 
