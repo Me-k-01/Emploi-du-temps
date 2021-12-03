62688cd55a8480ec13c2a0be5475b90473b08e50
Me-k-01 <mecaman01@gmail.com>
2021-12-03 18:28:28 +0100

    icon

--- index.php
+++ index.php
@@ -3,40 +3,55 @@
   <head>
     <meta charset="utf-8">
     <title>Emploi du temps</title>
+    <!-- Stylesheet -->
     <link rel="shortcut icon" href="favicon.png">
     <link rel="stylesheet" type="text/css" href="/css/master.css">
     <link rel="stylesheet" type="text/css" href="/css/table.css">
     <link rel="stylesheet" type="text/css" href="/css/color.css">
     <link rel="stylesheet" type="text/css" href="/css/scrollbar.css">
-    <link rel="stylesheet" type="text/css" href="/css/user-input.css">
+    <link rel="stylesheet" type="text/css" href="/css/configurator.css">
+    <!-- Scripts -->
+    <script src="https://kit.fontawesome.com/da84674338.js" crossorigin="anonymous"></script>
     <script src="/js/preset.js" type="text/javascript" charset="utf-8" defer></script>
     <script src="/js/config.js" type="text/javascript" charset="utf-8" defer></script>
     <script src="/js/table.js" type="text/javascript" charset="utf-8" defer></script>
     <script src="/php/data.php" type="text/javascript" charset="utf-8" defer></script>
     <script src="/js/main.js" type="text/javascript" charset="utf-8" defer></script>
   </head>
-  <body>
-    <!-- <header>
-      <h1>Emploi du temps</h1>
-    </header> -->
+  <body>  
     <main>
       <body>
         <table id="schedule"></table>
       </body>
-      <div id="ui" >
+      <div id="config" >
         <section>
           <h2>Inclure</h2>
           <ul>
-            <li><input class="include-title" type="text" value=""><input class="include-group" type="text" value=""><button class="del" type="button" name="button">del</button></li>
-            <li><input class="include-title" type="text" value=""><input class="include-group" type="text" value=""><button id="add" type="button" name="button">add</button></li>
+            <li>
+              <input class="include-title" type="text" value="">
+              <input class="include-group" type="text" value="">
+              <button class="del" type="button" name="button">
+                <i class="fas fa-trash"></i>
+              </button>
+            </li>
+            <li>
+              <input class="include-title" type="text" value="">
+              <input class="include-group" type="text" value="">
+              <button id="add" type="button" name="button">
+                <i class="fas fa-plus"></i>
+              </button>
+              </li>
           </ul>
         </section>
         <section>
-          <h2>
-            Exclure
-          </h2>
+          <h2>Exclure</h2>
           <ul>
-            <li><input class="exclude-title" type="text" value=""></li>
+            <li>
+              <input class="exclude-title" type="text" value="">
+              <button class="del" type="button" name="button">
+                <i class="fas fa-trash"></i>
+              </button>
+            </li>
           </ul>
         </section>
       </div>
