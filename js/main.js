118ff5d59071beb18a9a2fe6c8fda4ad7efaa839
Me-k-01 <mecaman01@gmail.com>
2021-12-03 19:17:43 +0100

    afficher / retirer le configurateur

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ const table = new Table(document.getElementById('schedule'), new Config('21L3-IN
 table.fill(matters);
 
 window.addEventListener('resize', () => table.update());
+
+document.getElementById('toggle-config').addEventListener('click', () => {
+
+  const conf = document.getElementById('config');
+  conf.className = conf.className === 'visible' ? '': 'visible';
+});
